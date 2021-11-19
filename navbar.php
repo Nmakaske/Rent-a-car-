bd2b441f113e98116f7039ff2895fdc0103ef9c6
MohammedDahman <83067387+MohammedDahman@users.noreply.github.com>
2021-11-19 10:36:17 +0100

    Navbar

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Navigation Bar</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initaial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="navbar.css">
+</head>
+<body>
+
+	<nav>
+		<div class="logo">
+			<p>rent a car</p>
+		</div>
+		<ul>
+			<li><a href="" class="active">home</a></li>
+			<li><a href="">reserveren</a></li>
+			<li><a href="">contact</a></li>
+			<li><a href="">inloggen/registreren</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<img src="image/img one.jpg">
+	</div>
+
+</body>
+</html>
